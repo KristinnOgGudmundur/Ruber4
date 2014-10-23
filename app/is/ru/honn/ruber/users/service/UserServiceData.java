d8885df5c805269b6534c7fdeb1f0f4d028412b0
gudmundurste12 <gudmundurste12@ru.is>
2014-10-23 16:42:22 +0000

    Indented UserServiceData.java

--- app/is/ru/honn/ruber/users/service/UserServiceData.java
+++ app/is/ru/honn/ruber/users/service/UserServiceData.java
@@ -10,33 +10,33 @@ import java.util.logging.Logger;
 public class UserServiceData implements UserService
 {
 
-  private Logger log = Logger.getLogger(UserServiceData.class.getName());
+	private Logger log = Logger.getLogger(UserServiceData.class.getName());
 
-  private UserDataGateway userDataGateway;
+	private UserDataGateway userDataGateway;
 
-  public UserServiceData(UserDataGateway userDataGateway)
-  {
-    this.userDataGateway = userDataGateway;
-  }
+	public UserServiceData(UserDataGateway userDataGateway)
+	{
+		this.userDataGateway = userDataGateway;
+	}
 
-  @Override
-  public User userSignup(String username, String firstName, String lastName, String password, String email, Date registered) throws UsernameExistsException
-  {
-    User user = new User(username, firstName, lastName, password, email, registered);
-    int id = userDataGateway.addUser(user);
-    user.setId(id);
-    return user;
-  }
+	@Override
+	public User userSignup(String username, String firstName, String lastName, String password, String email, Date registered) throws UsernameExistsException
+	{
+		User user = new User(username, firstName, lastName, password, email, registered);
+		int id = userDataGateway.addUser(user);
+		user.setId(id);
+		return user;
+	}
 
-  @Override
-  public User getUser(String username) throws UserNotFoundException
-  {
-    User user = userDataGateway.getUserByUsername(username);
-    if (user == null)
-    {
-      throw new UserNotFoundException("User not found: " + username);
-    }
-    return user;
-  }
+	@Override
+	public User getUser(String username) throws UserNotFoundException
+	{
+		User user = userDataGateway.getUserByUsername(username);
+		if (user == null)
+		{
+			throw new UserNotFoundException("User not found: " + username);
+		}
+		return user;
+	}
 
 }
