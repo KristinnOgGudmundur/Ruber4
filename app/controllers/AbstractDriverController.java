c87c1433b953e637f1e4cce1f55674fae094ab66
kristinnv12 <kristinnv12@ru.is>
2014-10-23 17:30:03 +0000

    git test

--- app/controllers/AbstractDriverController.java
+++ app/controllers/AbstractDriverController.java
@@ -2,10 +2,11 @@ package controllers;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import play.mvc.Controller;
 
 /**
  * Created by Gvendur Stefáns on 22.10.2014.
  */
-public abstract class AbstractDriverController {
+public abstract class AbstractDriverController extends Controller {
 	protected static ApplicationContext ctx = new FileSystemXmlApplicationContext("/conf/DriverService.xml");
 }
