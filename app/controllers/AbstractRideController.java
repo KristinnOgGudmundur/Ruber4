17c08588a604f5c47f6bdc123871e1f862d88301
gudmundurste12 <gudmundurste12@ru.is>
2014-10-28 18:32:50 +0000

    Made AbstractRideController abstract

--- app/controllers/AbstractRideController.java
+++ app/controllers/AbstractRideController.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 /**
  * Created by Gvendur Stefáns on 28.10.2014.
  */
-public class AbstractRideController {
+public abstract class AbstractRideController {
 	protected static ApplicationContext ctx = new FileSystemXmlApplicationContext("/conf/RideService.xml");
 	protected static ApplicationContext importCtx = new FileSystemXmlApplicationContext("/conf/ImportProcess.xml");
 }
