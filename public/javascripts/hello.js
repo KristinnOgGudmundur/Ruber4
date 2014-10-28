279a92a0a8796a0f63d2b16e888462a3d8948ee6
kristinnv12 <kristinnv12@ru.is>
2014-10-28 20:28:08 +0000

    refactoring

--- public/javascripts/hello.js
+++
@@ -1,5 +0,0 @@
-function show() {
-    document.getElementById('hiddebutton' ).style.display = "none";
-    document.getElementById('hiddeMe').style.display = "inline";
-    document.getElementById('border').style.border = "solid #808080";
-}
